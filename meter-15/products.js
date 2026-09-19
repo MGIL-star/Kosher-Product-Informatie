9350f2f391355b9a6f0b10f7a8ab09440342d9ad
MGIL-star <maor_granot@hotmail.com>
2026-09-19 22:16:54 +0200

    Align meter 15 product order with shelf photo

--- meter-15/products.js
+++ meter-15/products.js
@@ -49,6 +49,164 @@ window.KPI_PRODUCTS = [
     "reviewNote": "Online foto toont de Hebreeuwse 60g-verpakking. De volledig leesbare ingrediëntenlijst op foto 5 is overgenomen. Het gedeeltelijk zichtbare Britse tekstblok met percentage 25,4% moet nog volledig worden gecontroleerd. Het Badatz-keurmerk vermeldt dat zijn toezicht niet voor Pesach geldt."
   },
   {
+    "id": 3,
+    "brand": "Liel",
+    "name": "Briochebroodjes met Chocoladestukjes",
+    "variant": "280 g · 8 stuks · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444003329",
+    "ingredients": "Tarwebloem, chocoladestukjes (9,5%) (suiker, cacaomassa, cacaoboter, natuurlijk vanillearoma, emulgator: lecithinen uit koolzaad en zonnebloem), vers heel ei, suiker, invertsuikerstroop, plantaardige olie en vet (palm en koolzaad), natuurlijke aroma’s (bevatten alcohol), gist, tarwegluten, zout, emulgatoren (E471, E472e), aroma, verdikkingsmiddel (cellulosegom), gedeactiveerde gist, kleurstof (bètacaroteen).",
+    "allergens": [
+      "Tarwe (gluten)",
+      "Ei"
+    ],
+    "mayContain": "Melk.",
+    "kosher": "North West Kosher (NWK) - Afiyat Yisrael (Parve)",
+    "image": "images/liel-chocoladebroodjes-elisha.jpg",
+    "sourcePhotos": [
+      7
+    ],
+    "imageSource": "https://elisha.be/en/chocolate-chip-breads-rolls",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
+    "id": 5,
+    "brand": "Liel",
+    "name": "Croissants",
+    "variant": "320 g · 8 stuks · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444003305",
+    "ingredients": "Tarwebloem, plantaardige olie en vet (palm en koolzaad), suiker, water, gist, dextrose, tarwegluten, zout, natuurlijk aroma (bevat alcohol), vers heel ei, emulgatoren (E471, E472e), gedeactiveerde gist, conserveermiddel (calciumpropionaat), verdikkingsmiddel (cellulosegom), kleurstof (bètacaroteen).",
+    "allergens": [
+      "Tarwe (gluten)",
+      "Ei"
+    ],
+    "mayContain": "Melk.",
+    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
+    "image": "images/liel-croissants-elisha.jpg",
+    "sourcePhotos": [
+      9
+    ],
+    "imageSource": "https://elisha.be/en/french-croissants",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
+    "id": 4,
+    "brand": "Liel",
+    "name": "Briochebroodjes Naturel",
+    "variant": "280 g · 8 stuks · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444003336",
+    "ingredients": "Tarwebloem, vers heel ei, plantaardige olie en vet (palm en koolzaad), suiker, invertsuikerstroop, zout, tarwegluten, gist, natuurlijk aroma (bevat alcohol), emulgatoren (E471, E472e), aroma, conserveermiddel (calciumpropionaat), gedeactiveerde gist, verdikkingsmiddel (cellulosegom), kleurstof (bètacaroteen).",
+    "allergens": [
+      "Tarwe (gluten)",
+      "Ei"
+    ],
+    "mayContain": "Melk.",
+    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
+    "image": "images/liel-broodjes-elisha.jpg",
+    "sourcePhotos": [
+      8
+    ],
+    "imageSource": "https://elisha.be/en/breads-rolls",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
+    "id": 7,
+    "brand": "Liel",
+    "name": "Gesneden Brioche met Chocoladestukjes",
+    "variant": "500 g · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444003299",
+    "ingredients": "Tarwebloem, chocoladestukjes (10%) (suiker, cacaomassa, cacaoboter, natuurlijk vanillearoma, emulgator: lecithinen uit koolzaad en zonnebloem), suiker, vers heel ei, plantaardige olie en vet (palm en koolzaad), invertsuikerstroop, water, gist, zout, natuurlijk aroma (bevat alcohol), tarwegluten, aroma, gedeactiveerde gist, emulgator (E471), kleurstof (bètacaroteen).",
+    "allergens": [
+      "Tarwe (gluten)",
+      "Ei"
+    ],
+    "mayContain": "Melk.",
+    "kosher": "North West Kosher (NWK) - Afiyat Yisrael (Parve)",
+    "image": "images/liel-chocoladebrioche-elisha.jpg",
+    "sourcePhotos": [
+      11
+    ],
+    "imageSource": "https://elisha.be/en/sliced-french-brioche-with-chocolate-chips",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
+    "id": 6,
+    "brand": "Liel",
+    "name": "Gesneden Brioche Naturel",
+    "variant": "500 g · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444003282",
+    "ingredients": "Tarwebloem, suiker, vers heel ei, plantaardige olie en vet (palm en koolzaad), water, invertsuikerstroop, zout, gist, natuurlijk aroma (bevat alcohol), aroma, tarwegluten, conserveermiddel (calciumpropionaat), gedeactiveerde gist, emulgator (mono- en diglyceriden van vetzuren), kleurstof (bètacaroteen).",
+    "allergens": [
+      "Tarwe (gluten)",
+      "Ei"
+    ],
+    "mayContain": "Melk.",
+    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
+    "image": "images/liel-brioche-elisha.jpg",
+    "sourcePhotos": [
+      10
+    ],
+    "imageSource": "https://elisha.be/en/sliced-french-brioche",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
+    "id": 8,
+    "brand": "Liel",
+    "name": "Wit Sandwichbrood",
+    "variant": "550 g · 14 sneetjes · Mezonot",
+    "category": "Brood & brioche",
+    "ean": "3662444005576",
+    "ingredients": "Tarwebloem (63%), water, suiker, koolzaadolie, gist, natuurlijk aroma (bevat alcohol), zout, emulgator: E471, tuinbonenmeel, conserveermiddel: E282, tarwegluten, meelverbeteraar: E300.",
+    "allergens": [
+      "Tarwe (gluten)"
+    ],
+    "mayContain": "Melk, ei en sesam.",
+    "kosher": "Rabbi M. Weissmandl Badatz Beit Yosef - Afiyat Yisrael (Parve)",
+    "image": "images/liel-sandwichbrood.jpg",
+    "sourcePhotos": [
+      12,
+      13,
+      14
+    ],
+    "imageSource": "https://liel.fr/b-vienoiserie/19-pain-de-mie-nature-550g-liel-3662444005576.html",
+    "reviewNote": "Engelse ingrediëntenlijst op foto 13 gebruikt. Hebreeuwse lijst vermeldt ook dextrose. Foto 14: Rabbi M. Weissmandl en Badatz Beit Yosef; Pat/Afiyat Yisrael en zonder chadash-zorg.",
+    "imageBounds": [
+      0,
+      0,
+      1,
+      1
+    ]
+  },
+  {
     "id": 9,
     "brand": "Gusto",
     "name": "Pufuleti Maïssnacks met Zout",
@@ -85,163 +243,5 @@ window.KPI_PRODUCTS = [
     ],
     "imageSource": "https://uk.foodplus.eu/en_GB/p/Corn-Salted-Puffs-Pufuleti-Simpli-45g-Gusto/26419",
     "reviewNote": "Foto 18 toont voedingswaarden en barcode maar geen ingrediëntenlijst. Geen ingrediënten of allergenen van de 85 g-variant overgenomen."
-  },
-  {
-    "id": 3,
-    "brand": "Liel",
-    "name": "Briochebroodjes met Chocoladestukjes",
-    "variant": "280 g · 8 stuks · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444003329",
-    "ingredients": "Tarwebloem, chocoladestukjes (9,5%) (suiker, cacaomassa, cacaoboter, natuurlijk vanillearoma, emulgator: lecithinen uit koolzaad en zonnebloem), vers heel ei, suiker, invertsuikerstroop, plantaardige olie en vet (palm en koolzaad), natuurlijke aroma’s (bevatten alcohol), gist, tarwegluten, zout, emulgatoren (E471, E472e), aroma, verdikkingsmiddel (cellulosegom), gedeactiveerde gist, kleurstof (bètacaroteen).",
-    "allergens": [
-      "Tarwe (gluten)",
-      "Ei"
-    ],
-    "mayContain": "Melk.",
-    "kosher": "North West Kosher (NWK) - Afiyat Yisrael (Parve)",
-    "image": "images/liel-chocoladebroodjes-elisha.jpg",
-    "sourcePhotos": [
-      7
-    ],
-    "imageSource": "https://elisha.be/en/chocolate-chip-breads-rolls",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
-  },
-  {
-    "id": 4,
-    "brand": "Liel",
-    "name": "Briochebroodjes Naturel",
-    "variant": "280 g · 8 stuks · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444003336",
-    "ingredients": "Tarwebloem, vers heel ei, plantaardige olie en vet (palm en koolzaad), suiker, invertsuikerstroop, zout, tarwegluten, gist, natuurlijk aroma (bevat alcohol), emulgatoren (E471, E472e), aroma, conserveermiddel (calciumpropionaat), gedeactiveerde gist, verdikkingsmiddel (cellulosegom), kleurstof (bètacaroteen).",
-    "allergens": [
-      "Tarwe (gluten)",
-      "Ei"
-    ],
-    "mayContain": "Melk.",
-    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
-    "image": "images/liel-broodjes-elisha.jpg",
-    "sourcePhotos": [
-      8
-    ],
-    "imageSource": "https://elisha.be/en/breads-rolls",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
-  },
-  {
-    "id": 5,
-    "brand": "Liel",
-    "name": "Croissants",
-    "variant": "320 g · 8 stuks · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444003305",
-    "ingredients": "Tarwebloem, plantaardige olie en vet (palm en koolzaad), suiker, water, gist, dextrose, tarwegluten, zout, natuurlijk aroma (bevat alcohol), vers heel ei, emulgatoren (E471, E472e), gedeactiveerde gist, conserveermiddel (calciumpropionaat), verdikkingsmiddel (cellulosegom), kleurstof (bètacaroteen).",
-    "allergens": [
-      "Tarwe (gluten)",
-      "Ei"
-    ],
-    "mayContain": "Melk.",
-    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
-    "image": "images/liel-croissants-elisha.jpg",
-    "sourcePhotos": [
-      9
-    ],
-    "imageSource": "https://elisha.be/en/french-croissants",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
-  },
-  {
-    "id": 6,
-    "brand": "Liel",
-    "name": "Gesneden Brioche Naturel",
-    "variant": "500 g · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444003282",
-    "ingredients": "Tarwebloem, suiker, vers heel ei, plantaardige olie en vet (palm en koolzaad), water, invertsuikerstroop, zout, gist, natuurlijk aroma (bevat alcohol), aroma, tarwegluten, conserveermiddel (calciumpropionaat), gedeactiveerde gist, emulgator (mono- en diglyceriden van vetzuren), kleurstof (bètacaroteen).",
-    "allergens": [
-      "Tarwe (gluten)",
-      "Ei"
-    ],
-    "mayContain": "Melk.",
-    "kosher": "Manchester Beth Din Mehadrin - Afiyat Yisrael (Parve)",
-    "image": "images/liel-brioche-elisha.jpg",
-    "sourcePhotos": [
-      10
-    ],
-    "imageSource": "https://elisha.be/en/sliced-french-brioche",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
-  },
-  {
-    "id": 7,
-    "brand": "Liel",
-    "name": "Gesneden Brioche met Chocoladestukjes",
-    "variant": "500 g · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444003299",
-    "ingredients": "Tarwebloem, chocoladestukjes (10%) (suiker, cacaomassa, cacaoboter, natuurlijk vanillearoma, emulgator: lecithinen uit koolzaad en zonnebloem), suiker, vers heel ei, plantaardige olie en vet (palm en koolzaad), invertsuikerstroop, water, gist, zout, natuurlijk aroma (bevat alcohol), tarwegluten, aroma, gedeactiveerde gist, emulgator (E471), kleurstof (bètacaroteen).",
-    "allergens": [
-      "Tarwe (gluten)",
-      "Ei"
-    ],
-    "mayContain": "Melk.",
-    "kosher": "North West Kosher (NWK) - Afiyat Yisrael (Parve)",
-    "image": "images/liel-chocoladebrioche-elisha.jpg",
-    "sourcePhotos": [
-      11
-    ],
-    "imageSource": "https://elisha.be/en/sliced-french-brioche-with-chocolate-chips",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
-  },
-  {
-    "id": 8,
-    "brand": "Liel",
-    "name": "Wit Sandwichbrood",
-    "variant": "550 g · 14 sneetjes · Mezonot",
-    "category": "Brood & brioche",
-    "ean": "3662444005576",
-    "ingredients": "Tarwebloem (63%), water, suiker, koolzaadolie, gist, natuurlijk aroma (bevat alcohol), zout, emulgator: E471, tuinbonenmeel, conserveermiddel: E282, tarwegluten, meelverbeteraar: E300.",
-    "allergens": [
-      "Tarwe (gluten)"
-    ],
-    "mayContain": "Melk, ei en sesam.",
-    "kosher": "Rabbi M. Weissmandl Badatz Beit Yosef - Afiyat Yisrael (Parve)",
-    "image": "images/liel-sandwichbrood.jpg",
-    "sourcePhotos": [
-      12,
-      13,
-      14
-    ],
-    "imageSource": "https://liel.fr/b-vienoiserie/19-pain-de-mie-nature-550g-liel-3662444005576.html",
-    "reviewNote": "Engelse ingrediëntenlijst op foto 13 gebruikt. Hebreeuwse lijst vermeldt ook dextrose. Foto 14: Rabbi M. Weissmandl en Badatz Beit Yosef; Pat/Afiyat Yisrael en zonder chadash-zorg.",
-    "imageBounds": [
-      0,
-      0,
-      1,
-      1
-    ]
   }
 ];
